33836795a1891da363e72b23367e1a9d7ac73cad
Swapnil Kasliwal <skasliwal12@gmail.com>
2016-03-14 05:12:24 -0400

    redis problem with helpers

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,27 +1,27 @@
-module ItemsHelper
-  require 'json'
-  @@items = JSON.load $redis.get("items")
+# module ItemsHelper
+#   require 'json'
+#   @@items = JSON.load $redis.get("items")
   
-  def self.fetch_items
-    items =  $redis.get("items")
-    if items.nil?
-      items = Item.all.to_json
-      $redis.set("items", items)
-    end
-    @items = JSON.load items
-  end
+#   def self.fetch_items
+#     items =  $redis.get("items")
+#     if items.nil?
+#       items = Item.all.to_json
+#       $redis.set("items", items)
+#     end
+#     @items = JSON.load items
+#   end
   
-  def self.return_image(id)
-    if (id != 0)
-      begin
-        "https://d10bybrdwoa4y6.cloudfront.net/test" + @@items.find {|item| item["id"] == id}["img_url"]
-      rescue
-        "https://d10bybrdwoa4y6.cloudfront.net/test/assets/items_bg2.png"
-      end
-    else
-      "https://d10bybrdwoa4y6.cloudfront.net/test/assets/items_bg2.png"
-    end
-  end
+#   def self.return_image(id)
+#     if (id != 0)
+#       begin
+#         "https://d10bybrdwoa4y6.cloudfront.net/test" + @@items.find {|item| item["id"] == id}["img_url"]
+#       rescue
+#         "https://d10bybrdwoa4y6.cloudfront.net/test/assets/items_bg2.png"
+#       end
+#     else
+#       "https://d10bybrdwoa4y6.cloudfront.net/test/assets/items_bg2.png"
+#     end
+#   end
 
-end
+# end
 
